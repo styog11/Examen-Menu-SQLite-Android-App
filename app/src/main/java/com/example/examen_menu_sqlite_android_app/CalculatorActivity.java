bc574e60a4bf1448ea64e519dfc9b14737f43736
styog11 <driss.eladama@usmba.ac.ma>
2024-03-08 22:00:54 +0100

    initialiser CalculatorActivity  et TemperatureActivity

--- app/src/main/java/com/example/examen_menu_sqlite_android_app/CalculatorActivity.java
+++ app/src/main/java/com/example/examen_menu_sqlite_android_app/CalculatorActivity.java
@@ -3,12 +3,37 @@ package com.example.examen_menu_sqlite_android_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
-public class CalculatorActivity extends AppCompatActivity {
-
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
+TextView calculatorBoard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        GridLayout pads = findViewById(R.id.pads);
+        calculatorBoard=findViewById(R.id.resultat);
+        for (int i=0 ; i<pads.getChildCount();i++){
+            pads.getChildAt(i).setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button=(Button) view;
+        switch (button.getText().toString()){
+            case "=":
+                // evaluer
+                // ...
+                break;
+            case "AC":
+                calculatorBoard.setText("0");
+                break;
+            default:
+                calculatorBoard.setText(calculatorBoard.getText()+button.getText().toString());
+        }
     }
 }
