5861f9efad7008c4a54a1ca4b4e4eba01f0609de
styog11 <driss.eladama@usmba.ac.ma>
2024-03-08 21:36:43 +0100

    initialiser CalculatorActivity  et TemperatureActivity

---
+++ app/src/main/java/com/example/examen_menu_sqlite_android_app/TemperatureActivity.java
@@ -0,0 +1,14 @@
+package com.example.examen_menu_sqlite_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TemperatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temperature);
+    }
+}
